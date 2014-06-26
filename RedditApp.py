67a36ade1bf00ae972575c4e46aa349910379a53
Andrew Aodh Zhao <aodhzhao@gmail.com>
2014-06-25 19:45:18 -0700

    Using praw, connects to Reddit, gets last 5 submitted content of a hardcoded in user

--- RedditApp.py
+++ RedditApp.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python
 
-import tkinter as tk
+import Tkinter as tk
+import praw
+import xml.etree.cElementTree as ET
 
-class RedditApp(tk.Frame): #RedditApp inherits from tk.Frame
+class RedditApp(tk.Frame):
+
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
         self.createWidgets()
+        
+        # Make a connection to reddit and get the user
+        self.r = praw.Reddit(user_agent='Reddit App made by Aodh')
+        self.user = self.r.get_redditor('Unidan')         
 
     def createWidgets(self):
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
         self.quitButton.grid(column=1)
         
         self.label1 = tk.Label(self, text='Label')
-        self.label1.grid(column=0, row=0)
-        self.label2 = tk.Label(self, text='Label')
-        self.label2.grid(column=0, row=1)
-        self.label3 = tk.Label(self, text='Label')
-        self.label3.grid(column=0, row=2)
-        self.label4 = tk.Label(self, text='Label')
-        self.label4.grid(column=0, row=3)
+        self.label1.bind('<Button-1>', self.setupUserData)
+        self.label1.grid()
 
+    def setupUserData(self, event):  
+        gen = self.user.get_submitted(limit=5)
+        for x in gen:
+            print x
 
-if __name__ == '__main__':
+# Main application entry point
+def main():
     app = RedditApp()
     app.master.title('RedditApp')
     app.mainloop()
+    
+# Standard main function
+if __name__ == '__main__':
+       main()
