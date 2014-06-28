bac5c399c89bed0d70a16eac80faf5a8ab33be62
Andrew Aodh Zhao <aodhzhao@gmail.com>
2014-06-28 10:33:18 -0700

    Restructured app. Removed RedditApp class. No longer using praw, will use josn and requests to interact with Reddit API

--- RedditApp.py
+++ RedditApp.py
@@ -1,43 +1,35 @@
 #!/usr/bin/env python
 
 import Tkinter as tk
-import praw
-import xml.etree.cElementTree as ET
+import requests
+import json
 
-class RedditApp(tk.Frame):
+def createWidgets():
+    quitButton = tk.Button(text='Quit', command=quit)
+    quitButton.grid(column=3,row=2)
+    
+    usernameInput = tk.Entry();
+    usernameInput.insert(0, 'Type Username Here');
+    usernameInput.grid(column=1,row=1)
+    
+    passwordInput = tk.Entry()
+    passwordInput.insert(0, 'Type Password Here');
+    passwordInput.grid(column=1,row=2)
+    
+    button = tk.Button(text='Login')
+    button.bind('<Button-1>', 
+            lambda event: login(event, usernameInput.get(), passwordInput.get()))
+    button.grid(column=2,row=2)
 
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid()
-        self.createWidgets()
-        
-        # Make a connection to reddit
-        self.r = praw.Reddit(user_agent='Reddit App made by Aodh')    
-
-    def createWidgets(self):
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.quitButton.grid(column=1)
-        
-        self.usernameInput = tk.Entry(self);
-        self.usernameInput.insert(0, 'Type Username Here');
-        self.usernameInput.grid(column=2)
-        
-        self.button = tk.Button(self, text='Login')
-        self.button.bind('<Button-1>', self.setupUserData)
-        self.button.grid(column=3)
-
-    def setupUserData(self, event): 
-        username = self.usernameInput.get()
-        self.user = self.r.get_redditor(username)
-        
-        gen = self.user.get_submitted(limit=5)
-        for x in gen:
-            print x
+def login(event, username, password):
+    print username
 
 # Main application entry point
 def main():
-    app = RedditApp()
+    app = tk.Frame()
     app.master.title('RedditApp')
+    app.grid()
+    createWidgets() 
     app.mainloop()
     
 # Standard main function
