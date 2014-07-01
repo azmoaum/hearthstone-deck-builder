ebebd0932b0fce62c5e56b7d6f2bfd925f57bba8
Andrew Aodh Zhao <aodhzhao@gmail.com>
2014-06-30 22:00:40 -0700

    Click buttons to display first 8 alphabetical cards from different classes. A lot of valuesa re currently hardcoded in

--- HSDeckBuilder.py
+++ HSDeckBuilder.py
@@ -8,6 +8,8 @@ from PIL import Image, ImageTk
 from StringIO import StringIO
 import os
 
+HERO_CLASSES = ['Druid', 'Mage', 'Warlock', 'Hunter', 'Priest', 'Paladin', 'Rogue']
+
 class HSDeckBuilder(Frame):
     def __init__(self, session, master=None):
         Frame.__init__(self, master)
@@ -25,13 +27,14 @@ class HSDeckBuilder(Frame):
             card.grid(column=x, row=2)
             self.card_spot_list.append(card)
         
-        self.druid_button = Button(text="Druid", image=None)
-        self.druid_button.grid(column=0, row=0)
-        self.druid_button.bind('<Button-1>', 
-                lambda event: load_cards(session, 'Mage', self.card_spot_list))
-
+        for x, name in enumerate(HERO_CLASSES):
+            button = Button(text=name, image=None)
+            button.grid(column=x, row=0)
+            def handler(event, session=session, name=name, card_spot_list=self.card_spot_list):
+                return load_cards(event, session, name, self.card_spot_list)
+            button.bind('<Button-1>', handler)
     
-def load_cards(session, playerClass, card_spot_list):
+def load_cards(event, session, playerClass, card_spot_list):
     card_data = open("AllSets.enUS.json").read()
     card_data = json.loads(card_data)
     count = 0
@@ -40,21 +43,24 @@ def load_cards(session, playerClass, card_spot_list):
             if item['playerClass'] == playerClass:
                 print item['name'], ' ', item['id']
                 url = r'http://wow.zamimg.com/images/hearthstone/cards/enus/original/%s.png' % item['id']
+                response = session.get(url)
+                if response.status_code != 200:
+                    continue
                 display_image(session, url, card_spot_list, count)
                 count = count + 1
                 if count == len(card_spot_list):
                     return
     
 def display_image(session, url, card_spot_list, count):
-    print 'click'
     response = session.get(url)
-    img = Image.open(StringIO(response.content))
+    img = Image.open(StringIO(response.content)).resize((200, 304),Image.ANTIALIAS)
     img2 = ImageTk.PhotoImage(img)
     card_spot_list[count].config(image = img2)
     card_spot_list[count].image = img2
     
 #Main application entry point
 def main():
+    print HERO_CLASSES
     session = Session()
     session.headers = {'user-agent': 'HearthStone Deck Builder v0.1'}
     app = HSDeckBuilder(session)
