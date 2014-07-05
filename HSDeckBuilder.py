e15f39ce4feaacf69021b3366f8462a05496917c
Andrew Aodh Zhao <aodhzhao@gmail.com>
2014-07-04 20:09:24 -0700

    Page switching functionality added (within classes only)

--- HSDeckBuilder.py
+++ HSDeckBuilder.py
@@ -2,62 +2,80 @@
 
 from Tkinter import *
 from requests import Session
-import json
-import sys
 from PIL import Image, ImageTk
 from StringIO import StringIO
+import json
+import sys
 import os
-
-HERO_CLASSES = ['Druid', 'Mage', 'Warlock', 'Hunter', 'Priest', 'Paladin', 'Rogue', 'Shaman']
-CARD_SETS = ['Basic', 'Expert']
-CARDS_TO_IGNORE = ['CS2_013t', 'EX1_573t', 'EX1_158t', 'EX1_160t', 'CS2_017', 'EX1_165t2', 'EX1_165t1',
-                    'EX1_173', 'CS2_034', 'tt_010a', 'CS2_mirror', 'EX1_323h', 'EX1_317t', 'EX1-tk33', 
-                    'CS2_056', 'EX1_323w', 'EX1_554t', 'EX1_538t', 'EX1_534t', 'DS1h_292', 'NEW1_034', 
-                    'NEW1_033', 'NEW1_032', 'EX1_345t', 'CS1h_001', 'EX1_625t2', 'EX1_625t', 'EX1_130a', 
-                    'CS2_101t', 'CS2_101', 'EX1_383t', 'NEW1_006', 'EX1_131t', 'CS2_082', 'CS2_083b']
-
+import Config
 class HSDeckBuilder(Frame):
     def __init__(self, session, master=None):
         Frame.__init__(self, master)
         self.master.title('HSDeckBuilder')
+        self.pack_propagate(0)
         self.create_widgets(session)
         
     def create_widgets(self, session):
-        self.card_spot_list = []
-        for x in range(0, 4):
-            card = Label(text="Card_Spot", image=None)
-            card.grid(column=x, row=1)
-            self.card_spot_list.append(card)
-        for x in range(0, 4):
-            card = Label(text="Card_Spot", image=None)
-            card.grid(column=x, row=2)
-            self.card_spot_list.append(card)
-            
+        self.card_holder_list = []
         cards = load_cards(session)
-        for x, hero in enumerate(HERO_CLASSES):
+        
+        #Create Card Holders
+        for x in range(1, Config.MAX_ROWS_PER_PAGE+1):
+            for y in range(0, Config.MAX_CARDS_PER_ROW):
+                frame = LabelFrame(width=Config.CARD_WIDTH, height=Config.CARD_HEIGHT)
+                frame.grid_propagate(False)
+                frame.grid(column=y, row=x)
+                card_holder = Label(frame, image=None)
+                card_holder.grid()
+                self.card_holder_list.append(card_holder)
+        
+        #Create Hero Buttons
+        for x, hero in enumerate(Config.HERO_CLASSES):
             button = Button(text=hero, image=None)
             button.grid(column=x, row=0)
             def handler(event, session=session, cards=cards, hero=hero):
-                return self.display_cards(event, session, cards, hero)
+                self.current_hero = hero
+                self.current_page = 0
+                return self.display_cards(event, session, cards)
             button.bind('<Button-1>', handler)
-            
-        button = Button(text='Back')
-        button.grid(column=8,row=2)
         
+        #Create Next Button
         button = Button(text='Next')
         button.grid(column=9,row=2)
-    
-    def display_cards(self, event, session, cards, hero):
-        count = 0
-        for card in cards[hero]:
-            self.display_card(card, count)
-            count = count + 1
-            if count == len(self.card_spot_list):
-                return    
+        def handler(event, session=session, cards=cards):
+            if self.current_page >= Config.MAX_PAGES-1:
+                return
+            self.current_page = self.current_page+1
+            return self.display_cards(event, session, cards)
+        button.bind('<Button-1>', handler)
         
-    def display_card(self, card, count):
-        self.card_spot_list[count].config(image = card['image'])
-        self.card_spot_list[count].image = card['image']
+        #Create Back Button
+        button = Button(text='Back')
+        button.grid(column=8,row=2)
+        def handler(event, session=session, cards=cards):
+            if self.current_page <= 0:
+                return
+            self.current_page = self.current_page-1
+            return self.display_cards(event, session, cards)
+        button.bind('<Button-1>', handler)
+        
+    def display_cards(self, event, session, cards):
+        cards = cards[self.current_hero][Config.MAX_CARDS_PER_PAGE*self.current_page:]
+        num_cards = len(cards)
+        if num_cards > Config.MAX_CARDS_PER_PAGE:
+            cards_to_display = Config.MAX_CARDS_PER_PAGE
+        else:
+            cards_to_display = num_cards
+            
+        for holder_num in range(0, Config.MAX_CARDS_PER_PAGE):
+            if holder_num < cards_to_display:
+                self.display_card(cards[holder_num]['image'], holder_num) 
+            else:
+                self.display_card('', holder_num)
+        
+    def display_card(self, card_image, holder_num):
+        self.card_holder_list[holder_num].config(image = card_image)
+        self.card_holder_list[holder_num].image = card_image
     
 def load_cards(session):
     cards = {}
@@ -67,23 +85,34 @@ def load_cards(session):
     card_data = open("AllSets.enUS.json").read()
     card_data = json.loads(card_data)
 
-    for hero in HERO_CLASSES:
+    for hero in Config.HERO_CLASSES:
         print 'Loading cards for %s...' % hero
-        for set in CARD_SETS:
+        for set in Config.CARD_SETS:
             for card in card_data[set]:
                 if 'playerClass' in card and 'cost' in card:
                     if card['playerClass'] == hero:
-                        if card['id'] in CARDS_TO_IGNORE:
+                    #
+                    #
+                    #
+                    #
+                    #
+                    # if card does not have a playerclass than it is a netural card!
+                    #
+                    #
+                    #
+                    #
+                    #
+                        if card['id'] in Config.CARDS_TO_IGNORE:
                             continue
                         url = r'http://wow.zamimg.com/images/hearthstone/cards/enus/original/%s.png' % card['id']
                         response = session.get(url)
                         if response.status_code != 200:
                             continue
-                        img = Image.open(StringIO(response.content)).resize((200, 304),Image.ANTIALIAS)
+                        img = Image.open(StringIO(response.content)).resize((Config.CARD_WIDTH, Config.CARD_HEIGHT),Image.ANTIALIAS)
                         photo_img = ImageTk.PhotoImage(img)
                         card['image'] = photo_img
                         card_list.append(card)
-                        
+                
         card_list.sort(key=lambda x:(x['cost'], x['name']))
         cards[hero] = card_list[:]
         #for card in cards[hero]:
