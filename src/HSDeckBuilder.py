7d34ef8349010de3e25f3077ee46cf50ecfe4631
Andrew Aodh Zhao <aodhzhao@gmail.com>
2014-07-12 17:19:04 -0700

    Added functionality to save decks

--- src/HSDeckBuilder.py
+++ src/HSDeckBuilder.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import json
+import os.path
 import Config
 from MainWidgets import HeroButtonsFrame, CardsFrame, DeckFrame, DeckLabel, CardLabel
-from Tkinter import Frame, Button
+from Tkinter import Frame, Button, Entry
 from requests import Session
 from PIL import Image, ImageTk
 from StringIO import StringIO
@@ -44,13 +45,10 @@ class HSDeckBuilder(Frame):
                                     height=Config.CARD_LABEL_HEIGHT*2)
         self.deck_frame.grid_propagate(False)
         self.deck_frame.grid(column=3, row=1, rowspan=2)
-        
-        self.deck_label = DeckLabel(master=self.deck_frame, image=None)
-        self.deck_label.grid()
                 
         #Create HeroButtonsFrame
         self.hero_buttons = HeroButtonsFrame(master=self.master)
-        self.hero_buttons.grid(row=0, column=1, columnspan=Config.MAX_CARDS_PER_ROW)
+        self.hero_buttons.grid(column=1, row=0, columnspan=Config.MAX_CARDS_PER_ROW)
         
         #Add buttons to HeroButtonsFrame
         for x, hero in enumerate(Config.HERO_CLASSES):
@@ -91,11 +89,15 @@ class HSDeckBuilder(Frame):
         self.back_button.bind('<Button-1>', handler)
         self.back_button.grid(column=0,row=1, rowspan=2)
         
+        #Create Save Button
         self.save_button = Button(master=self.master, text='Save Deck')
         def handler(event):
             return self.save_deck(event)
         self.save_button.bind('<Button-1>', handler)
         self.save_button.grid(column=3,row=4)
+        
+        self.deck_name_entry = Entry(master=self.master, text='Deck name')
+        self.deck_name_entry.grid(column=3,row=0)
         
     def display_card_images(self, card_dict, event=None):
         #card_list=List of cards to display depending on the current hero and page
@@ -128,6 +130,9 @@ class HSDeckBuilder(Frame):
         if card_names.count(card.name) == 2:
             return
         
+        #Create Deck Label
+        #Check if deck already contains card being added. If so,
+        #add "x2" to the label. Otherwise, add the card.
         deck_label = DeckLabel(master=self.deck_frame, image=None, text=card.name)
         if card.name in card_names:
            for label in deck_labels:
@@ -138,20 +143,33 @@ class HSDeckBuilder(Frame):
         self.deck_frame.deck_list.append(card.name)
     
     def save_deck(self, event):
+        #Check if deck is complete
         if self.deck_frame.deck_list < Config.MAX_CARDS_IN_DECK:
             return
-        str = ' \n'.join(self.deck_frame.deck_list)
-        with open("..\decks\Decks.txt", "w") as myFile:
-            myFile.write(str)
+        deck_name = self.deck_name_entry.get()
         
+        #Create a path for the deck. If the path exists, rename it.
+        path = '%s\%s.txt' % (Config.DECK_PATH, deck_name)
+        count = 1
+        while os.path.isfile(path):
+            path = '%s\%s-00%d.txt' % (Config.DECK_PATH, deck_name, count)
+            count = count + 1
+        
+        #Create content of deck file and write it
+        content_name = 'Deck: %s\n\n' % (deck_name)
+        content_deck = ['    -'+s for s in self.deck_frame.deck_list]
+        content_deck = '\n'.join(content_deck)
+        with open(path, 'a') as myFile:
+            myFile.write(content_name)
+            myFile.write(content_deck)
         
 def load_cards(session):
     card_dict = {}
     card_list = []
     
     #Read in the card data from json file
-    
-    card_data = open("..\json\AllSets.enUS.json").read()
+    path = '%s\AllSets.enUS.json' % (Config.JSON_PATH)
+    card_data = open(path).read()
     card_data = json.loads(card_data)
 
     for hero in Config.HERO_CLASSES:
@@ -164,7 +182,7 @@ def load_cards(session):
                     #
                     # if card does not have a playerclass than it is a netural card!
                     #
-                        url = r'http://wow.zamimg.com/images/hearthstone/cards/enus/original/%s.png' % card['id']
+                        url = '%s/%s.png' % (Config.IMAGES_URL, card['id'])
                         response = session.get(url)
                         if response.status_code == 200:
                             img = Image.open(StringIO(response.content)).resize((Config.CARD_LABEL_WIDTH, 
